756dbd750dd4906633a03fa7c09c75d65c985842
MKI <mki@softwareag.com>
2022-06-08 14:09:37 +0530

    Adding k6 to the execution.

--- tests/k6/functionalTests.js
+++ tests/k6/functionalTests.js
@@ -1,6 +1,14 @@
 import { sleep } from"k6";
 import http from "k6/http";
-import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export let options = {
+  duration: "1m",
+  vus: 5,
+  thresholds: {
+    http_req_duration: ["p(95)<500"] // 95 percent of response times must be below 500ms
+  }
+};
 
 export default function() {
   http.get(`${__ENV.API_URL}`)	
@@ -9,7 +17,6 @@ export default function() {
 
 export function handleSummary(data) {
   return {
-   'stdout': textSummary(data),
    'junit.xml': jUnit(data),
   };
 }
