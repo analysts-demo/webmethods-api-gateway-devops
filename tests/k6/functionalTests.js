ff3a867140511d016782e31a3fc9e9e4d6f7c9d8
MKI <mki@softwareag.com>
2022-06-08 19:44:49 +0530

    Added the API and the promotion related details.

--- tests/k6/functionalTests.js
+++ tests/k6/functionalTests.js
@@ -1,18 +1,22 @@
 import { check,sleep,fail } from"k6";
+import { Counter} from 'k6/metrics';
 import http from "k6/http";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export const CounterErrors = new Counter('Errors');
+
+export const options = {
+  thresholds: {
+    'Errors': ['count>0'],
+  },
+};
+
 
 export default function() {
   let res;
   res =  http.get(`${__ENV.API_URL}`)
-   if (
-    !check(res, {
-      'status code MUST be 200': (res) => res.status == 200,
-    })
-   ) {
-    fail('status code was *not* 200');
-  }
-  sleep(3);
+  const contentNOK = res.status === 200;
+  CounterErrors.add(!contentNOK);
 };
 
 export function handleSummary(data) {
