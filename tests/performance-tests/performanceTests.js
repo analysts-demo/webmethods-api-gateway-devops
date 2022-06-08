567d4bc2859d3b1280733f24a817315d4cccf408
MKI <mki@softwareag.com>
2022-06-08 12:20:11 +0530

    Adding k6 to the execution.

--- tests/performance-tests/performanceTests.js
+++ tests/performance-tests/performanceTests.js
@@ -3,7 +3,7 @@ import http from "k6/http";
 
 export let options = {
   duration: "1m",
-  vus: 50,
+  vus: 5,
   thresholds: {
     http_req_duration: ["p(95)<500"] // 95 percent of response times must be below 500ms
   }
