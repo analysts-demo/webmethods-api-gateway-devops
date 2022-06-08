3039d0985cc3cdd1c9af74fc43935a3e1ffdac97
MKI <mki@softwareag.com>
2022-06-08 13:55:48 +0530

    Adding k6 to the execution.

--- tests/k6/functionalTests.js
+++ tests/k6/functionalTests.js
@@ -2,12 +2,6 @@ import { sleep } from"k6";
 import http from "k6/http";
 import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
-export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0'], // http errors should be less than 0%
-  },
-};
-
 export default function() {
   http.get(`${__ENV.API_URL}`)	
   sleep(3);
