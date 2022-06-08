b8928afedf1ecd5564771cc4cf11b47082962b35
MKI <mki@softwareag.com>
2022-06-08 13:58:45 +0530

    Adding k6 to the execution.

--- tests/k6/functionalTests.js
+++ tests/k6/functionalTests.js
@@ -9,7 +9,7 @@ export default function() {
 
 export function handleSummary(data) {
   return {
-   'stdout': textSummary(data, { indent: ' ', enableColors: true }),
+   'stdout': textSummary(data),
    'junit.xml': jUnit(data),
   };
 }
